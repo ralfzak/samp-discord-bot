722e18bb2910069243280f6396c32839f4158594
ralfzak <ralfzak@gmail.com>
2020-05-10 22:13:52 +0200

    Add unit tests

---
+++ tests/Helpers/StringHelperTest.cs
@@ -0,0 +1,19 @@
+﻿using main.Helpers;
+using Xunit;
+
+namespace tests.Helpers
+{
+    public class StringHelperTest
+    {
+        [Fact]
+        public void Test_ComputeLevenshteinDistance_WithSimilarStrings_RetunsSame()
+        {
+            var stringA = "AnyString";
+            var stringB = "AnyString";
+
+            var result = StringHelper.ComputeLevenshteinDistance(stringA, stringB);
+
+            Assert.Equal(1, result);
+        }
+    }
+}
