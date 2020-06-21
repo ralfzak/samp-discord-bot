46aeb143634b2a1c926b2453e594ae2eb87f9498
ralfzak <ralfzak@gmail.com>
2020-06-21 20:22:55 +0200

    Add MysqlBansRepositoryTest

---
+++ test/Core/Database/FakeDatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using main.Core.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace test.Core.Database
+{
+    public class FakeDatabaseContext<T> where T: class
+    {
+        private readonly DbContextOptions<DatabaseContext> _contextOptions;
+        
+        protected FakeDatabaseContext()
+        {
+            _contextOptions = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase("database")
+                .Options;
+        }
+
+        protected DatabaseContext Context() =>
+            new DatabaseContext(_contextOptions);
+        
+        protected List<T> GetAll(DatabaseContext context) => 
+            context.Set<T>().ToList();
+
+        protected void Provision(DatabaseContext context, List<T> records)
+        {
+            context.Set<T>().RemoveRange(GetAll(context));
+            context.SaveChanges();
+
+            if (records != null)
+            {
+                context.Set<T>().AddRange(records);
+                context.SaveChanges();
+            }
+        }
+    }
+}
