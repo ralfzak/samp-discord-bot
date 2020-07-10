3df70429f9df7f320b7413daab0646bfa5a537ef
Ralf Zakarian <ralfzak97@gmail.com>
2020-07-10 18:22:03 +0200

    Fix concurrent database access exceptions (#40)

--- main/Program.cs
+++ main/Program.cs
@@ -60,7 +60,7 @@ namespace main
                 .AddDbContext<DatabaseContext>(options =>
                 {
                     options.UseMySQL(DbConnectionString());
-                })
+                }, ServiceLifetime.Transient)
                 .AddSingleton<IBansRepository, MysqlBansRepository>()
                 .AddSingleton<IVerificationsRepository, MysqlVerificationsRepository>()
                 
