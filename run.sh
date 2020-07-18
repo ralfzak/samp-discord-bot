2c342f06d7489c17a0b676b3ea4d3f634587752e
Ralf Zakarian <hello@ralfzak.me>
2020-07-18 17:10:45 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DOCKER_OWNER="docker_username"
+export DOCKER_PASSWORD="docker_password"
+export HOME_DIRECTORY="/path/to/dirs"
+export BOT_TOKEN="bot_token"
+export DATABASE_PASSWORD="database_password"
+
+docker-compose up -d
